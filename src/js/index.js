1ca9f79c750a235114045d3904b6bab79824f042
ruytereu-arch <ruyter.eu@gmail.com>
2026-05-31 14:18:04 +0100

    feat: renderiza profile card; adiciona mensagem de carregamento

--- src/js/index.js
+++ src/js/index.js
@@ -12,39 +12,42 @@ btnSearch.addEventListener('click', async () => {
     if (userName) {
 
     try {
+        profileResults.innerHTML = `<p class="loading">Carregando...</p>`;
             
         // Aqui você pode chamar as funções para buscar os dados do usuário
     
         const response = await fetch(`${baseURL}/users/${userName}`);
 
         if (!response.ok) {
+            profileResults.innerHTML = "";
             alert(`usuário ${userName} não encontrado. Verifique o nome do usuário e tente novamente.`);
             return;
         }
 
         const user_profile = await response.json();
 
-        const user_avatar = user_profile.avatar_url
-        const user_name = user_profile.name
-        const user_bio = user_profile.bio
-        const user_followers = user_profile.followers
-        const user_following = user_profile.following
+        // const user_avatar = user_profile.avatar_url
+        // const user_name = user_profile.name
+        // const user_bio = user_profile.bio
+        // const user_followers = user_profile.followers
+        // const user_following = user_profile.following
 
         // console.log(`name: ${user_name}; followers: ${user_followers}; following: ${user_following}`); // para validar obtenção de dados
 
         profileResults.innerHTML = `
         <div class="profile-card">
-            <img src="${user_avatar}" alt="avatar de ${user_name}"
+            <img src="${user_profile.avatar_url}" alt="avatar de ${user_profile.name}"
             class="profile-avatar">
 
             <div class="profile-info">
-                <h2>${user_name}</h2>
-                <p>${user_bio || "Biografia não disponível"}</p>
+                <h2>${user_profile.name}</h2>
+                <p>${user_profile.bio || "Biografia não disponível"}</p>
             </div>
             
         </div>`;
         
     }catch(error) {
+        profileResults.innerHTML = "";
         console.error(`Erro ao buscar perfil do usuário`, error);
         alert(`Ocorreu um erro ao buscar o perfil do usuário. Por favor, tente novamente mais tarde.`);
 
@@ -65,5 +68,3 @@ btnSearch.addEventListener('click', async () => {
 //         // Aqui você pode chamar as funções para buscar os dados do usuário
 //     }
 // });
-
-
